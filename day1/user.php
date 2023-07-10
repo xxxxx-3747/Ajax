1a282ba3ed2040c036fbca596057fda80a47dd07
xxxxx-3747 <1921708164@qq.com>
2023-07-10 20:47:18 +0800

    ajax初始化2

---
+++ day1/user.php
@@ -0,0 +1,48 @@
+<?php
+
+//设置head
+header('Content_type: application/json');
+
+
+$data = array(
+    array(
+        'id'=>1,
+        'name'=>'大傻子',
+        'age'=>18
+    ),
+    array(
+        'id'=>2,
+        'name'=>'二傻子',
+        'age'=>19
+    ),
+    array(
+        'id'=>3,
+        'name'=>'小傻子',
+        'age'=>20
+    ),
+    array(
+        'id'=>4,
+        'name'=>'麻瓜',
+        'age'=>20
+    )
+);
+
+// var_dump($data);
+
+//转化为json格式
+//判断id是否存在
+if(empty($_GET['id'])){
+
+    //没有获取到具体的id,打印全部的数据
+    $json = json_encode($data);
+    var_dump($json);
+}else{
+    foreach ($data as $key) {
+        if($key['id'] != $_GET['id']){
+            continue;
+        }
+        $json = json_encode($key);
+        echo $json;
+    }
+
+}
