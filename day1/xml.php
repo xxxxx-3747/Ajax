6131fc9ecd15d3e4213fdeb9160ff3f642e04094
xxxxx-3747 <1921708164@qq.com>
2023-07-18 18:11:38 +0800

    table和xml

---
+++ day1/xml.php
@@ -0,0 +1,12 @@
+<?php
+header('Content-Type: application/xml');
+header("Access-Control-Allow-Origin: *");
+?>
+<?xml version="1.0" encoding="utf-8"?>
+<person>
+    <name>cx</name>
+    <age>18</age>
+    <sex>
+        男
+    </sex>
+</person>
